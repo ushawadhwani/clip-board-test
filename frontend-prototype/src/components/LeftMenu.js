cafcbb6c6074f614f9fa4842b7f9ce68a47c7e0d
techitreesoutions <Libra270177@>
2021-03-24 15:43:17 +0530

    leftmenu

--- frontend-prototype/src/components/LeftMenu.js
+++ frontend-prototype/src/components/LeftMenu.js
@@ -1,11 +1,11 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircle } from "@fortawesome/free-regular-svg-icons";
+import { faTumblr } from "@fortawesome/free-brands-svg-icons";
 
 export default function LeftMenu() {
   return (
     <div class="sidebar">
-      {/* <FontAwesomeIcon icon={faTumblr} style={{ color: "#ffffff" }} /> */}
-
+      <FontAwesomeIcon icon={faTumblr} style={{ color: "#ffffff" }} />
       <FontAwesomeIcon icon={faCircle} style={{ color: "#ffffff" }} />
     </div>
   );
